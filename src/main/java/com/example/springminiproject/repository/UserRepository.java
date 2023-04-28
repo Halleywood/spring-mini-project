bfc7b8b241b7bb1795715f7cd7307bb1789d8d19
Kelsey Halley <kelseyghalley@gmail.com>
2023-04-28 13:08:58 -0700

    made UserRepository methods

--- src/main/java/com/example/springminiproject/repository/UserRepository.java
+++ src/main/java/com/example/springminiproject/repository/UserRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    //will need at least two methods for service method to call.
+    //returns a true or false if the email address the user enters during registration is already in database
+    boolean existsByEmailAddress(String emailAddress);
+
+    //user enters email at sign in, uses this email to locate the user object
+    User findUserByEmailAddress(String emailAddress);
 }
