9afd053eeb5f35d531d112b15ca88ea05fd09d04
Kelsey Halley <kelseyghalley@gmail.com>
2023-04-28 12:23:42 -0700

    created UserProfile, connected via JPA

--- src/main/java/com/example/springminiproject/model/UserProfile.java
+++ src/main/java/com/example/springminiproject/model/UserProfile.java
@@ -1,8 +1,73 @@
 package com.example.springminiproject.model;
 
-public class UserProfile {
+import javax.persistence.*;
 
+@Entity
+@Table(name="profiles")
+public class UserProfile {
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String profileDescription;
+    //.....................................................................CONSTRUCTORS
+
+    public UserProfile() {}
+
+    public UserProfile(Long id, String firstName, String lastName, String profileDescription) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profileDescription = profileDescription;
+    }
     //..........................................................ONE USER TO ONE PROFILE
-    //........................................................ONE USER TO MANY CHICKENS
+
     //..........................................................GETTERS/SETTERS/TO STRING
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public void setProfileDescription(String profileDescription) {
+        this.profileDescription = profileDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "Id=" + Id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", profileDescription='" + profileDescription + '\'' +
+                '}';
+    }
 }
